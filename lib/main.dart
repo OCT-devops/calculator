239b77d01cd2c9e6aa23411fd534a559f42f2034
OCT-devops <ralfhreneil12@gmail.com>
2024-11-18 07:40:45 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Calculator',
+      theme: ThemeData.dark(),
+      home: const CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String number1 = "";
+  String number2 = "";
+  String operand = "";
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                reverse: true,
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    "$number1$operand$number2".isEmpty
+                        ? "0"
+                        : "$number1$operand$number2",
+                    style: const TextStyle(
+                        fontSize: 48, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+              ),
+            ),
+            Wrap(
+              children: Btn.buttonValues
+                  .map(
+                    (value) => SizedBox(
+                      width: value == Btn.n0
+                          ? screenSize.width / 2
+                          : (screenSize.width / 4),
+                      height: screenSize.width / 5,
+                      child: buildButton(value),
+                    ),
+                  )
+                  .toList(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(value) {
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: Material(
+        color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
+        shape: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Colors.white24,
+            ),
+            borderRadius: BorderRadius.circular(20)),
+        child: InkWell(
+          onTap: () => btnTap(value),
+          child: Center(
+              child: Text(
+            value,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          )),
+        ),
+      ),
+    );
+  }
+
+  void btnTap(String value) {
+    if (value == Btn.delete) {
+      delete();
+      return;
+    }
+
+    if (value == Btn.clear) {
+      clearAll();
+      return;
+    }
+
+    if (value == Btn.modulo) {
+      convertToPercent();
+      return;
+    }
+
+    if (value == Btn.calculate) {
+      calculate();
+      return;
+    }
+
+    appendValue(value);
+  }
+
+  void calculate() {
+    if (number1.isEmpty) return;
+    if (operand.isEmpty) return;
+    if (number2.isEmpty) return;
+
+    double num1 = double.parse(number1);
+    double num2 = double.parse(number2);
+
+    var result = 0.0;
+    switch (operand) {
+      case Btn.addition:
+        result = num1 + num2;
+        break;
+      case Btn.subtraction:
+        result = num1 - num2;
+        break;
+      case Btn.multiplication:
+        result = num1 * num2;
+        break;
+      case Btn.division:
+        result = num1 / num2;
+        break;
+      default:
+    }
+
+    setState(() {
+      number1 = "$result";
+
+      if (number1.endsWith(".0")) {
+        number1 = number1.substring(0, number1.length - 2);
+      }
+
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  void convertToPercent() {
+    if (number1.isNotEmpty && operand.isNotEmpty && number2.isNotEmpty) {
+      calculate();
+    }
+
+    if (operand.isNotEmpty) {
+      return;
+    }
+
+    final number = double.parse(number1);
+    setState(() {
+      number1 = "${(number / 100)}";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  void clearAll() {
+    setState(() {
+      number1 = "";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  void delete() {
+    if (number2.isNotEmpty) {
+      number2 = number2.substring(0, number2.length - 1);
+    } else if (operand.isNotEmpty) {
+      operand = "";
+    } else if (number1.isNotEmpty) {
+      number1 = number1.substring(0, number1.length - 1);
+    }
+
+    setState(() {});
+  }
+
+  void appendValue(String value) {
+    if (value != Btn.dot && int.tryParse(value) == null) {
+      if (operand.isNotEmpty && number2.isNotEmpty) {
+        calculate();
+      }
+      operand = value;
+    } else if (number1.isEmpty || operand.isEmpty) {
+      if (value == Btn.dot && number1.contains(Btn.dot)) return;
+      if (value == Btn.dot && (number1.isEmpty || number1 == Btn.n0)) {
+        value = "0.";
+      }
+      number1 += value;
+    } else if (number2.isEmpty || operand.isNotEmpty) {
+      if (value == Btn.dot && number2.contains(Btn.dot)) return;
+      if (value == Btn.dot && (number2.isEmpty || number2 == Btn.n0)) {
+        value = "0.";
+      }
+      number2 += value;
+    }
+
+    setState(() {});
+  }
+
+  Color getBtnColor(value) {
+    return [
+      Btn.division,
+      Btn.multiplication,
+      Btn.subtraction,
+      Btn.addition,
+      Btn.modulo,
+    ].contains(value)
+        ? const Color.fromARGB(255, 123, 124, 124)
+        : [
+            Btn.delete,
+            Btn.clear,
+            Btn.calculate,
+          ].contains(value)
+            ? Colors.blueAccent
+            : Colors.black12;
+  }
+}
+
+class Btn {
+  static const String addition = "+";
+  static const String subtraction = "-";
+  static const String multiplication = "*";
+  static const String division = "/";
+  static const String modulo = "%";
+  static const String calculate = "=";
+  static const String clear = "C";
+  static const String delete = "D";
+
+  static const String n1 = "1";
+  static const String n2 = "2";
+  static const String n3 = "3";
+  static const String n4 = "4";
+  static const String n5 = "5";
+  static const String n6 = "6";
+  static const String n7 = "7";
+  static const String n8 = "8";
+  static const String n9 = "9";
+  static const String n0 = "0";
+  static const String dot = ".";
+
+  static const List<String> buttonValues = [
+    delete,
+    clear,
+    modulo,
+    multiplication,
+    n7,
+    n8,
+    n9,
+    division,
+    n4,
+    n5,
+    n6,
+    subtraction,
+    n1,
+    n2,
+    n3,
+    addition,
+    n0,
+    dot,
+    calculate,
+  ];
+}
